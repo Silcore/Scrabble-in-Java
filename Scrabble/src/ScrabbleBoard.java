8738af90ddd5f7393afc3da53af830b4711322b3
Dexter Ketchum <dexterk97@hotmail.com>
2018-04-05 19:19:07 -0400

    Refactoring
    Combined InfoPanel and ButtonPanel into ScrabbleBoard. Put ScrabbleBoard into ScrabbleFrame. Scrabble is reserved for management of game data.

---
+++ Scrabble/src/ScrabbleBoard.java
@@ -0,0 +1,82 @@
+// Filipe Guerra and Dexter Ketchum
+// COP3252 - Java
+// Semester Project (Scrabble.java)
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
+
+public class ScrabbleBoard extends JPanel {
+	public ScrabbleBoard() {
+		super();
+
+		InfoPanel iPanel = new InfoPanel();
+		ButtonPanel bPanel = new ButtonPanel();
+		
+		super.setLayout(new BorderLayout());
+		super.add(bPanel, BorderLayout.NORTH);
+		super.add(iPanel, BorderLayout.WEST);
+	}
+}
+
+class InfoPanel extends JPanel {
+	public InfoPanel() {
+		super();
+		
+		// Initializing infoBox and scrabbleBox Labels
+		JLabel infoBox = new JLabel("<html>Letter Distribution<br/><pre>A-9  J-1  S-4"
+				+ "<br/>B-2  K-1  T-6<br/>C-2  L-4  U-4<br/>D-4  M-2  V-2"
+				+ "<br/>E-12 N-6  W-2<br/>F-2  O-8  X-1<br/>G-3  P-2  Y-2"
+				+ "<br/>H-2  Q-1  Z-1<br/>I-9  R-6<br/>BLANK-2</pre></html>", SwingConstants.CENTER);
+		JLabel scrabbleBox = new JLabel("<html>S<sub>1</sub><br/>C<sub>3</sub>"
+				+ "<br/>R<sub>1</sub><br/>A<sub>1</sub><br/>B<sub>3</sub>"
+				+ "<br/>B<sub>3</sub><br/>L<sub>1</sub><br/>E<sub>1</sub>"
+				+ "<br/></html>", SwingConstants.CENTER);
+		infoBox.setFont(new Font("Serif", Font.PLAIN, 12));
+		scrabbleBox.setFont(new Font("Serif", Font.PLAIN, 12));
+		
+		// Formatting Panel
+		super.setLayout(new BorderLayout());
+		super.setBorder(BorderFactory.createCompoundBorder(new MatteBorder(0, 0, 0, 2, Color.BLACK), new EmptyBorder(10, 10, 10, 10)));
+		super.add(infoBox, BorderLayout.PAGE_END);
+		super.add(scrabbleBox, BorderLayout.PAGE_START);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		this.setBackground(Color.WHITE);
+	}
+}
+
+class ButtonPanel extends JPanel {
+	public ButtonPanel() {
+		super();
+		
+		// Initializing endTurn Button
+		JButton endTurn = new JButton("End Turn");
+		endTurn.setFocusPainted(false);
+		endTurn.setContentAreaFilled(false);
+		endTurn.setOpaque(true);
+		endTurn.setBackground(Color.CYAN);
+		
+		// Formatting Panel
+		super.setLayout(new BorderLayout());
+		super.setBorder(BorderFactory.createCompoundBorder(new MatteBorder(0, 0, 2, 0, Color.BLACK), new EmptyBorder(10, 10, 10, 10)));
+		super.add(endTurn, BorderLayout.LINE_END);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		this.setBackground(Color.WHITE);
+	}
+}
---
+++ Scrabble/src/ScrabbleFrame.java
@@ -0,0 +1,21 @@
+// Filipe Guerra and Dexter Ketchum
+// COP3252 - Java
+// Semester Project (Scrabble.java)
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class ScrabbleFrame extends JFrame {
+	public ScrabbleFrame() {
+		super("Scrabble in Java");
+		super.setLayout(new BorderLayout());
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		super.add(new ScrabbleBoard());
+		
+		super.setMinimumSize(new Dimension(800, 500));
+		super.setSize(new Dimension(800, 500));
+		super.setVisible(true);
+	}
+}
